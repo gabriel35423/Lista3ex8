ca057d2308e259e9c9abe576d16ef2b382a2d7da
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-06-21 22:11:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cont = 0;
+            double result = 0;
+
+            do
+            {
+                cont++;
+                result = result + cont;
+
+            } while (cont < 100);
+
+            Console.WriteLine("A Soma dos Números Inteiros Positivos no Intervalo de 1 a 100 é de: {0}", result);
+        }
+    }
+}
